1667354b514adde29cfd30ddc4d07062ab324728
jfcp <jfcp@inesingenieros.com>
2018-06-06 13:13:42 +0200

    add template

--- src/containers/layouts/PrivateLayout/index.js
+++ src/containers/layouts/PrivateLayout/index.js
@@ -7,15 +7,50 @@ import { Switch, Route } from 'react-router-dom'
 import Template from '../../Template'
 
 
+class Header extends Component {
+    render() {
+        return (
+            <div className="Header"></div>
 
+        )
+    }
+}
+class Footer extends Component {
+    render() {
+        return (
+            <div className="Footer"></div>
+
+        )
+    }
+}
 /* PrivateLayout component definition */
 const PrivateLayout = ({ component: Component, authed, ...rest }) => {
     return (
         <Route
             {...rest}
-            render={(props) => authed === true
-                ? <Template> <Component {...props} /> </Template>
-                : <Redirect to={{ pathname: '/', state: { from: props.location } }} />}
+            render={(props) => {
+
+                if (authed === true) {
+                    return (
+                        <div className="PrivateLayout">
+                            <header>
+                                <Header />
+                            </header>
+                            <main>
+                                <Template> <Component {...props} /> </Template>
+                            </main>
+                            <footer>
+                                <Footer />
+                            </footer>
+                        </div>
+                    )
+
+                } else {
+
+                    return (<Redirect to={{ pathname: '/', state: { from: props.location } }} />)
+                }
+            }
+            }
         />
     )
 }
