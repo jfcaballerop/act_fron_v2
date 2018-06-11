86a69cbeffe913007ef64b0fab5710d77cad02d1
jfcaballero <jfcp@inesingenieros.com>
2018-06-11 22:46:32 +0200

    add history

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { createHistory } from 'history'
+import { createHistory, useBasename } from 'history'
+import createBrowserHistory from 'history/createBrowserHistory'
 
-const history = useRouterHistory(createHistory)({
-	basename: '/'
-})
+
 
 // Routes
 import App from "./containers/App";
 
 import registerServiceWorker from './registerServiceWorker';
 
+// const history = useRouterHistory(createHistory)({
+// 	basename: '/'
+// })
+const history = createBrowserHistory({
+	basename: '/'
+})
 
 ReactDOM.render(
+
 	<Router history={history}>
 		<App />
 	</Router>,
